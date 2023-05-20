7df65519ff68fc13c26e4f76ea875cf41de03c79
Omar <devopsomar@gmail.com>
2023-05-20 21:15:35 +0400

    crush

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import AddProject from './client/AddProject'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
